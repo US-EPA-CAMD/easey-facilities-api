0a47d2864b1b18b020eeddc3bf60f0872bd39a14
Junie-CVP <juniesenthilkumar@cvpcorp.com>
2020-12-03 17:11:15 -0500

    Static data for getFacilities

--- src/facilities/facilities.repository.ts
+++ src/facilities/facilities.repository.ts
@@ -1,22 +1,35 @@
 import { Repository, EntityRepository } from "typeorm";
 
-
 import { FacilityDTO } from './dto/facility.dto';
 import { Plant } from './entities/plant.entity';
+import { LinkDTO } from './dto/link.dto';
 
+function createLinks(facId: number): LinkDTO[] {
+  const links: Array<LinkDTO> = [
+    new LinkDTO("self", `/facilities/${facId}`),
+    new LinkDTO("units", `/facilities/${facId}/units`),
+    new LinkDTO("stacks", `/facilities/${facId}/stacks`),
+    new LinkDTO("owners", `/facilities/${facId}/owners`),
+    new LinkDTO("contacts", `/facilities/${facId}/contacts`),
+  ]
+
+  return links;
+}
 
 const facilities: Array<FacilityDTO> = [
-  new FacilityDTO(1, 3, "Barry", null, "AL"),
-  new FacilityDTO(2, 9, "Copper Station", null, "TX"),
-  new FacilityDTO(3, 51, "Dolet Hills Power Station", null, "LA"),
-  new FacilityDTO(4, 87, "Escalante", null, "NM"),
-  new FacilityDTO(5, 108, "Holcomb", null, "KS"),
-  new FacilityDTO(6, 113, "Cholla", null, "AZ"),
-  new FacilityDTO(7, 130, "Cross", null, "SC"),
-  new FacilityDTO(8, 477, "Valmont", null, "CO"),
-  new FacilityDTO(9, 564, "Curtis H. Stanton Energy Center", null, "FL"),
-  new FacilityDTO(10, 596, "Madison Street", null, "DE"),
+  new FacilityDTO(1, 3, "Barry", "AL", createLinks(1)),
+  new FacilityDTO(2, 9, "Copper Station", "TX", createLinks(2)),
+  new FacilityDTO(3, 51, "Dolet Hills Power Station", "LA", createLinks(3)),
+  new FacilityDTO(4, 87, "Escalante", "NM", createLinks(4)),
+  new FacilityDTO(5, 108, "Holcomb", "KS", createLinks(5)),
+  new FacilityDTO(6, 113, "Cholla", "AZ", createLinks(6)),
+  new FacilityDTO(7, 130, "Cross", "SC", createLinks(7)),
+  new FacilityDTO(8, 477, "Valmont", "CO", createLinks(8)),
+  new FacilityDTO(9, 564, "Curtis H. Stanton Energy Center", "FL", createLinks(9)),
+  new FacilityDTO(10, 596, "Madison Street", "DE", createLinks(10)),
 ];
+
+
 
 @EntityRepository(Plant)
 export class FacilityRepository extends Repository<Plant>{
