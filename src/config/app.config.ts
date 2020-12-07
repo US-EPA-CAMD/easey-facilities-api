91dc44f9807968051fb56e4ed74dbbb6980c8b03
jasonwhitehead <jasonwhitehad@cvpcorp.com>
2020-12-06 23:23:23 -0600

    additional devops setup & CaC improvements

---
+++ src/config/app.config.ts
@@ -0,0 +1,20 @@
+import { registerAs } from '@nestjs/config';
+
+const title = 'Facility Management';
+const path = 'api/facility-mgmt';
+const host = process.env.HOST || 'localhost';
+const port = process.env.PORT || 8000;
+
+let uri = `https://${host}/${path}`
+
+if (host == 'localhost') {
+  uri = `http://localhost:${port}/${path}`;
+}
+
+export default registerAs('app', () => ({
+  title,
+  path,
+  host,
+  port,
+  uri,
+}));
