30c484459981f5915ae6b2231747235ebbf1a756
Kyle Herceg <kdherceg@email.wm.edu>
2021-10-20 16:41:31 -0400

    Added-Cors-Config

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,16 +1,17 @@
 import { registerAs } from '@nestjs/config';
 
-const path = process.env.EASEY_FACILITIES_API_PATH || 'api/facility-mgmt'
+const path = process.env.EASEY_FACILITIES_API_PATH || 'api/facility-mgmt';
 const host = process.env.EASEY_FACILITIES_API_HOST || 'localhost';
 const port = process.env.EASEY_FACILITIES_API_PORT || 8080;
 
-let uri = `https://${host}/${path}`
+let uri = `https://${host}/${path}`;
 
 if (host == 'localhost') {
   uri = `http://localhost:${port}/${path}`;
 }
 
 export default registerAs('app', () => ({
+  name: 'facilities-api',
   title: process.env.EASEY_FACILITIES_API_TITLE || 'Facilities Management',
   path,
   host,
--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { CorsOptionsService } from '@us-epa-camd/easey-common/cors-options';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+  const corsOptionsService = app.get(CorsOptionsService);
 
+  const appName = configService.get<string>('app.name');
   const appTitle = configService.get<string>('app.title');
   const appPath = configService.get<string>('app.path');
   const appEnv = configService.get<string>('app.env');
@@ -19,18 +22,17 @@ async function bootstrap() {
   let swaggerCustomOptions = null;
 
   if (appEnv != 'production') {
-    appDesc = `EPA ${appEnv} Environment: The content on this page is not production data and used for <strong>development</strong> and/or <strong>testing</strong> purposes only.`
+    appDesc = `EPA ${appEnv} Environment: The content on this page is not production data and used for <strong>development</strong> and/or <strong>testing</strong> purposes only.`;
     swaggerCustomOptions = {
-      customCss: '.description .renderedMarkdown p { color: #FC0; padding: 10px; background: linear-gradient(to bottom,#520001 0%,#6c0810 100%); }'
+      customCss:
+        '.description .renderedMarkdown p { color: #FC0; padding: 10px; background: linear-gradient(to bottom,#520001 0%,#6c0810 100%); }',
     };
   }
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.setGlobalPrefix(appPath);
-  app.enableCors({
-    origin: '*',
-    exposedHeaders: '*',
-    methods: '*',
+  app.enableCors(async (req, callback) => {
+    await corsOptionsService.configure(req, appName, callback);
   });
 
   const swaggerDocOptions = new DocumentBuilder()
@@ -40,10 +42,17 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, swaggerDocOptions);
-  SwaggerModule.setup(`${appPath}/swagger`, app, document, swaggerCustomOptions);
+  SwaggerModule.setup(
+    `${appPath}/swagger`,
+    app,
+    document,
+    swaggerCustomOptions,
+  );
 
   await app.listen(configService.get<number>('app.port'));
-  console.log(`Application is running on: ${await app.getUrl()}/${appPath}/swagger`);
+  console.log(
+    `Application is running on: ${await app.getUrl()}/${appPath}/swagger`,
+  );
 }
 
 bootstrap();
