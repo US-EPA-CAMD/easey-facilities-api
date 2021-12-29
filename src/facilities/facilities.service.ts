8019a3c23a74d4eb63b6c197f9fdf0ec6df728dd
Kyle-Herceg <kyleherceg@cvpcorp.com>
2021-12-29 11:28:12 -0500

    Added additional error handling

--- src/facilities/facilities.service.ts
+++ src/facilities/facilities.service.ts
@@ -1,6 +1,10 @@
 import { Request } from 'express';
 import { Not, IsNull, FindManyOptions } from 'typeorm';
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Logger } from '@us-epa-camd/easey-common/logger';
 
@@ -32,38 +36,48 @@ export class FacilitiesService {
     @InjectRepository(FacilityUnitAttributesRepository)
     private readonly facilityUnitAttributesRepository: FacilityUnitAttributesRepository,
     private readonly facilityAttributesMap: FacilityAttributesMap,
-    private Logger: Logger,
+    private logger: Logger,
   ) {}
 
   async getFacilities(
     facilityParamsDTO: FacilityParamsDTO,
     req: Request,
   ): Promise<FacilityDTO[]> {
-    const { state, page, perPage } = facilityParamsDTO;
+    let results;
+    let totalCount;
 
-    const findOpts: FindManyOptions = {
-      select: ['id', 'facilityId', 'facilityName', 'state'],
-      order: {
-        id: 'ASC',
-      },
-    };
+    try {
+      this.logger.info('Getting facilities');
+      const { state, page, perPage } = facilityParamsDTO;
 
-    if (state) {
-      findOpts.where = { facilityId: Not(IsNull()), state: state };
-    } else {
-      findOpts.where = { facilityId: Not(IsNull()) };
+      const findOpts: FindManyOptions = {
+        select: ['id', 'facilityId', 'facilityName', 'state'],
+        order: {
+          id: 'ASC',
+        },
+      };
+
+      if (state) {
+        findOpts.where = { facilityId: Not(IsNull()), state: state };
+      } else {
+        findOpts.where = { facilityId: Not(IsNull()) };
+      }
+
+      if (page && perPage) {
+        findOpts.skip = (page - 1) * perPage;
+        findOpts.take = perPage;
+      }
+
+      [results, totalCount] = await this.facilitiesRepository.findAndCount(
+        findOpts,
+      );
+
+      ResponseHeaders.setPagination(page, perPage, totalCount, req);
+      this.logger.info('Got facilities');
+    } catch (e) {
+      this.logger.error(InternalServerErrorException, e.message, true);
     }
 
-    if (page && perPage) {
-      findOpts.skip = (page - 1) * perPage;
-      findOpts.take = perPage;
-    }
-
-    const [results, totalCount] = await this.facilitiesRepository.findAndCount(
-      findOpts,
-    );
-
-    ResponseHeaders.setPagination(page, perPage, totalCount, req);
     return this.facilityMap.many(results);
   }
 
@@ -71,7 +85,7 @@ export class FacilitiesService {
     const facility = await this.facilitiesRepository.findOne(id);
 
     if (facility === undefined) {
-      this.Logger.error(NotFoundException, 'Facility id does not exist', true, {
+      this.logger.error(NotFoundException, 'Facility id does not exist', true, {
         id: id,
       });
     }
@@ -83,15 +97,23 @@ export class FacilitiesService {
     facilityAttributesParamsDTO: FacilityAttributesParamsDTO,
     req: Request,
   ): Promise<FacilityAttributesDTO[]> {
-    const query = await this.facilityUnitAttributesRepository.getAllFacilityAttributes(
-      facilityAttributesParamsDTO,
-      req,
-    );
+    this.logger.info('Getting all facility attributes');
+    let query;
+    try {
+      query = await this.facilityUnitAttributesRepository.getAllFacilityAttributes(
+        facilityAttributesParamsDTO,
+        req,
+      );
+    } catch (e) {
+      this.logger.error(InternalServerErrorException, e.message, true);
+    }
 
     req.res.setHeader(
       'X-Field-Mappings',
       JSON.stringify(fieldMappings.facilities.attributes),
     );
+
+    this.logger.info('Got all facility attributes');
 
     return this.facilityAttributesMap.many(query);
   }
@@ -113,11 +135,20 @@ export class FacilitiesService {
       }
     }
 
-    const query = await this.programYearRepository.getApplicableFacilityAttributes(
-      applicableFacilityAttributesParamsDTO,
-      isArchived,
-      isUnion,
-    );
+    this.logger.info('Getting all applicable facility attributes');
+
+    let query;
+    try {
+      query = await this.programYearRepository.getApplicableFacilityAttributes(
+        applicableFacilityAttributesParamsDTO,
+        isArchived,
+        isUnion,
+      );
+    } catch (e) {
+      this.logger.error(InternalServerErrorException, e.message, true);
+    }
+
+    this.logger.info('Got all applicable facility attributes');
 
     return this.applicableFacilityAttributesMap.many(query);
   }
