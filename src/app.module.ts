f315b682e028b61e6e2201a8ebb3ec45ebb7bc4c
Jason Whitehead <jwhitehead@austin.rr.com>
2022-02-11 23:38:23 -0600

    cors cache control headers

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,13 @@ import { RouterModule } from 'nest-router';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import routes from './routes';
-import dbConfig from './config/db.config';
-import appConfig from './config/app.config';
-import { TypeOrmConfigService } from './config/typeorm.config';
-
+import { dbConfig } from '@us-epa-camd/easey-common/config';
 import { LoggerModule } from '@us-epa-camd/easey-common/logger';
 import { CorsOptionsModule } from '@us-epa-camd/easey-common/cors-options';
+
+import routes from './routes';
+import appConfig from './config/app.config';
+import { TypeOrmConfigService } from './config/typeorm.config';
 import { FacilitiesModule } from './facilities/facilities.module';
 
 @Module({
