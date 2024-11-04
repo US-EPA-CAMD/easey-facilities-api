7394d46479cccc6cad48fe9a856bc0fe64f150ea
ergjustin <justin.joseph.erg.com>
2024-11-04 01:53:04 -0600

    feature:added maintenance middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { HttpModule } from '@nestjs/axios';
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { dbConfig } from '@us-epa-camd/easey-common/config';
 import { CorsOptionsModule } from '@us-epa-camd/easey-common/cors-options';
 import { LoggerModule } from '@us-epa-camd/easey-common/logger';
 import { RouterModule } from 'nest-router';
+import { MaintenanceMiddleware } from '@us-epa-camd/easey-common/middleware/maintenance.middleware';
 
 import appConfig from './config/app.config';
 import { TypeOrmConfigService } from './config/typeorm.config';
@@ -52,4 +53,8 @@ import {
     IsUnitTypeValidator,
   ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(MaintenanceMiddleware).forRoutes({ path: '*', method: RequestMethod.ALL })
+  }
+}
