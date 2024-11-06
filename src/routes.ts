e8a0263ac1347af7b9d31e771e9f43327842f2e7
Jon Maxwell Diebold <max.diebold@erg.com>
2024-11-06 16:22:36 -0500

    fix(6421): updated `RouterModule` usage

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,10 @@
-import { Routes } from 'nest-router';
-
 import { FacilitiesWorkspaceModule } from './facilities-workspace/facilities.module';
 import { FacilitiesModule } from './facilities/facilities.module';
 import { UnitWorkspaceModule } from './unit-workspace/unit.module';
 import { StackPipeWorkspaceModule } from './stack-pipe-workspace/stack-pipe.module';
 import { UnitStackConfigurationWorkspaceModule } from './unit-stack-configuration-workspace/unit-stack-configuration.module';
 
-const routes: Routes = [
+const routes = [
   {
     path: '/facilities',
     module: FacilitiesModule,
