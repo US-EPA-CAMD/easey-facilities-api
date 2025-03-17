738f128519220afde8b98acd77e3fd869fa56f8c
Yonatan Amezene <yamezene@decisionpointcorp.com>
2025-03-17 16:10:48 -0400

    Add Additional database connection Settings #6620

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -27,6 +27,14 @@ export class TypeOrmConfigService implements TypeOrmOptionsFactory {
   }
 
   createTypeOrmOptions(): TypeOrmModuleOptions {
+
+    // Values are: true | false | 'all' | 'query', 'error', 'schema', 'warn', 'info', 'log'
+    const rawLogging = this.configService.get<string>('app.sqlLogging');
+    const sqlLogging: LoggerOptions = rawLogging === 'false'
+      ? false
+      : rawLogging.split(',').map(level => level.trim()) as LoggerOptions;
+
+
     return {
       type: 'postgres',
       applicationName: this.configService.get<string>('app.name'),
@@ -49,8 +57,8 @@ export class TypeOrmConfigService implements TypeOrmOptionsFactory {
         idle_in_transaction_session_timeout: this.configService.get<number>('app.idleInTransactionSessionTimeout'), // Terminates idle transactions after the specified time (in ms).
         maxUses: this.configService.get<number>('app.maxUsesBeforeRecreatingConnection'), //Recreate connections after n uses
       },
-      // Enable SQL Logging. Values are: true | false | 'all' | ['query', 'error', 'schema', 'warn', 'info', 'log']
-      logging: this.configService.get<LoggerOptions>('app.sqlLogging', ),
+      // Enable SQL Logging. Values are: true | false | 'all' | 'query', 'error', 'schema', 'warn', 'info', 'log'
+      logging: sqlLogging,
       // Logs queries exceeding this limit (does not terminate, 'statement_timeout' terminates them).
       maxQueryExecutionTime: this.configService.get<number>('app.maxQueryExecutionTime'),
     };
