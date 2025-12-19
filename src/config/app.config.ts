95e1e8880905a15439636d527e89122c3532326f
Jon Maxwell Diebold <max.diebold@erg.com>
2025-12-18 20:43:47 -0700

    feat(7027): add flag to toggle replica access

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -65,6 +65,9 @@ export default registerAs('app', () => ({
     'EASEY_FACILITIES_API_ENABLE_GLOBAL_VALIDATION_PIPE',
     true,
   ),
+  enableReplicaDbAccess: getConfigValueBoolean(
+    'EASEY_FACILITIES_API_ENABLE_REPLICA_DB_ACCESS',
+  ),
   version: getConfigValue('EASEY_FACILITIES_API_VERSION', 'v0.0.0'),
   published: getConfigValue('EASEY_FACILITIES_API_PUBLISHED', 'local'),
   enableRoleGuard: getConfigValueBoolean(
